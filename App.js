0762f285ef2d5064d7484bc3673167143caf5278
Pr0-Grammer <sajidzain445@gmail.com>
2024-02-14 21:42:13 +0500

    setup basic UI for a calculator

--- App.js
+++ App.js
@@ -1,20 +1,131 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const [displayValue, setDisplayValue] = useState('0');
+
+  const handleButtonPress = (value) => {
+    setDisplayValue(displayValue === '0' ? value : displayValue + value);
+  }
+
+  const clear = () => {
+    setDisplayValue('');
+  }
+
+  //function to add
+  const add = () => {
+    
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.display}>
+        <Text style={styles.displayText}>{displayValue}</Text>
+      </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('1')} >
+          <Text style={styles.buttonText}>1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('2')} >
+          <Text style={styles.buttonText}>2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('3')} >
+          <Text style={styles.buttonText}>3</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => clear()} >
+          <Text style={styles.buttonText}>C</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('4')} >
+          <Text style={styles.buttonText}>4</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('5')} >
+          <Text style={styles.buttonText}>5</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('6')} >
+          <Text style={styles.buttonText}>6</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress()} >
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('7')} >
+          <Text style={styles.buttonText}>7</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('8')} >
+          <Text style={styles.buttonText}>8</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('9')} >
+          <Text style={styles.buttonText}>9</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress()} >
+          <Text style={styles.buttonText}>-</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('0')} >
+          <Text style={styles.buttonText}>0</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress()} >
+          <Text style={styles.buttonText}>/</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress()} >
+          <Text style={styles.buttonText}>x</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress()} >
+          <Text style={styles.buttonText}>=</Text>
+        </TouchableOpacity>
+      </View>
+      
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+
     alignItems: 'center',
+    justifyContent: 'center', 
+
+    backgroundColor: '#36383d',
+  },
+
+  display: {
+    flex: 1,
+
+    backgroundColor: '#36383d',
+
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+
+    width: '100%',
+
+    padding: 10,
+  },
+
+  displayText: {
+    color: '#fff',
+    fontSize: 50,
+  },
+
+  buttons:{
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     justifyContent: 'center',
   },
+
+  button:{
+    width: '24%',
+    height: '24%',
+
+    margin: 0.5,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    backgroundColor: 'white',
+  },
+
+  buttonText:{
+    fontSize: 30,
+  }
+  
 });
